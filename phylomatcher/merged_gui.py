fd59eddc663e410452e06e390573951921d25b22
Matias Vantilburg <mvant@unc.edu>
2023-05-19 14:52:52 -0400

    Tmatch file_len change to make GUI work

--- phylomatcher/merged_gui.py
+++ phylomatcher/merged_gui.py
@@ -247,17 +247,14 @@ import pandas as pd
 import csv
 from tqdm import tqdm
 import os
-import subprocess
 from fuzzywuzzy import fuzz
 
 def file_len(fname):
-    p = subprocess.Popen(
-        ["wc", "-l", fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    result, err = p.communicate()
-    if p.returncode != 0:
-        raise IOError(err)
-    return int(result.strip().split()[0])
+    with open(fname, 'r') as file:
+        line_count = 0
+        for _ in file:
+            line_count += 1
+    return line_count
 
 
 def trait_main(traitfile, speciesfile, outfile, header):
